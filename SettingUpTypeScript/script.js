4efa3ee1c16bc47af19fc74c0ab5f97cc5d8e67c
Harshil Sharma <harshils3107@gmail.com>
2026-05-30 07:28:32 +0530

    initial commit

---
+++ SettingUpTypeScript/script.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//NOTE:ANY VALID JS CODE IS ALSO VALID TS CODE IN MAJORITY OF THE CASES!
+function abcd() {
+    console.log("Hello");
+}
+//# sourceMappingURL=script.js.map
