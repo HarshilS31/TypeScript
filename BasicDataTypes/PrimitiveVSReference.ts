25d99644d3846cc3be575a3bd1fceec1a66309ec
Harshil Sharma <harshils3107@gmail.com>
2026-05-30 07:39:01 +0530

    New Files added

---
+++ BasicDataTypes/PrimitiveVSReference.ts
@@ -0,0 +1,10 @@
+//PRIMITIVE:Number,boolean,String....
+//REFERENCE:Array,Object,Enums...
+let a = 12
+let b=a
+b+=12
+console.log(b)//25
+let c =[10,12,3321,1333]
+let d=c;
+d[2]=11999
+console.log(d)
